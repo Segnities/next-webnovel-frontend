07d7ff198ba4ba93c41976a59b61e84208424ce8
unknown <dimab787898@gmail.com>
2024-10-01 21:25:01 +0300

    fixed issue with parent layout

--- app/book/[slug]/(chapters)/chapter/[id]/layout.tsx
+++ app/book/[slug]/(chapters)/chapter/[id]/layout.tsx
@@ -1,7 +1,3 @@
-export const metadata = {  
-   layoutSegments: []
-}
-
 export default function ChapterLayout({
    children,
 }: Readonly<{
---
+++ app/book/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import BookActions from "@/components/book-actions";
+import BookAdditionalInfo from "@/components/book-additional-info";
+import BookChaptersList from "@/components/book-chapters-list";
+import BookImg from "@/components/book-img";
+import BookTitle from "@/components/book-title";
+import Header from "@/components/header";
+import BgLayout from "@/components/ui/bg-layout";
+import Footer from "@/components/ui/footer";
+
+export default function BookPage() {
+   return (
+      <>
+         <Header />
+         <BgLayout>
+            <div className="container mx-auto flex flex-col lg:flex-row gap-3">
+               <div className="lg:w-1/12 hidden lg:flex"></div>
+               <div className="lg:max-w-1/4 lg:w-1/4 pr-4">
+                  <BookImg />
+                  <BookActions />
+                  <BookAdditionalInfo />
+               </div>
+               <div className="lg:w-3/5">
+                  <BookTitle />
+                  <BookChaptersList />
+               </div>
+               <div className="lg:w-1/12 hidden lg:flex"></div>
+            </div>
+         </BgLayout>
+         <Footer />
+      </>
+   )
+}
